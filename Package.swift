c1b499d9b207ca902f3be0cd338c1492ab7b9484
Arpit Garg <arpit2005.garg@gmail.com>
2026-04-19 11:42:32 +0530

    feat: added module dependencies

--- Package.swift
+++ Package.swift
@@ -5,6 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "F5TTSMLX",
+    platforms: [
+        .macOS(.v14),
+        .iOS(.v17),
+        .tvOS(.v17),
+        .visionOS(.v1),
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -12,11 +18,18 @@ let package = Package(
             targets: ["F5TTSMLX"]
         ),
     ],
+    dependencies: [
+        .package(url: "https://github.com/ml-explore/mlx-swift", from: "0.31.0")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "F5TTSMLX"
+            name: "F5TTSMLX",
+            dependencies: [
+                .product(name: "MLX", package: "mlx-swift"),
+                .product(name: "MLXNN", package: "mlx-swift"),
+            ]
         ),
 
     ],
